965c85eac2f222292840346e50a81a21d42894ad
august-alt <77973983+august-alt@users.noreply.github.com>
2022-05-12 15:14:12 +0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Installation and Usage
+
+Install the latest Cookiecutter if you haven't installed it yet::
+
+    pip install -U cookiecutter
+
+Generate a preferences test::
+
+    cookiecutter https://github.com/august-alt/preferences_test_template.git
+
+Type in folder you want to create template in and a name of a widget to test.
---
+++ {{cookiecutter.folder_name}}/{{cookiecutter.widget_name|lower}}widgettest.cpp
@@ -0,0 +1,46 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2021 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#include "{{cookiecutter.widget_name|lower}}widgettest.h"
+
+#include <fstream>
+
+const std::string dataPath = "../../../../data/preferences/";
+
+namespace tests 
+{
+
+void {{cookiecutter.widget_name}}Test::test()
+{
+    std::ifstream file;
+
+    file.open(dataPath + "{{cookiecutter.widget_name|lower}}.xml", std::ifstream::in);
+    if (file.good()) 
+    {
+       
+    }
+
+    file.close();
+}
+
+}
+
+QTEST_MAIN(tests::{{cookiecutter.widget_name}}Test)
+
